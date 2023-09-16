35519cc9aef22a387db024625976f05b47b8cfc8
frankk-d <frankding2011@gmail.com>
2023-09-16 15:00:19 -0400

    Updates

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,42 +1,35 @@
 import Shop from "./Shop";
 import { Link } from "react-router-dom";
+import bread from "../Assets/Kawaii Bread.svg";
+import basket from "../Assets/Basket.svg";
+import heart from "../Assets/h-1.svg";
+import font from "../Assets/Alata-Regular.ttf";
+
+let score = 1090;
+
 export default function Home() {
-    let x = 9;
     return(
+        <div className="Desktop2 flex w-[100%] h-[100%] absolute bg-slate-400 overflow-hidden ">
+
+        <div className="Shop w-[6%] h-[12%] left-[92%] top-[2%] absolute align-middle">
+            <div className="Rectangle1 w-[100%] h-[100%] absolute bg-white rounded-3xl border border-white" />
+            <img className="asdd w-[7vw] h-[8vh] left-0 top-[15%] absolute" src={basket} />     
+
+        </div>
+        <div className="relative w-[100%] h-[100%] top-[91%] left-[80%]">
+            <img className="absolute w-[51px] h-[47px] top-0 left-0" alt="H" src={heart} />
+            <img className="absolute w-[51px] h-[47px] top-0 left-[63px]" alt="H" src={heart} />
+            <img className="absolute w-[51px] h-[47px] top-0 left-[127px]" alt="H" src={heart} />
+            <img className="absolute w-[51px] h-[47px] top-0 left-[190px]" alt="H" src={heart} />
+            <img className="absolute w-[51px] h-[47px] top-0 left-[253px]" alt="H" src={heart} />
+        </div>
+
         
-        <div className="Desktop2 w-[100%] h-[100%] absolute bg-slate-400 overflow-hidden">
-        <div className="Shop w-[116px] h-[110px] left-[90%] top-[5%] absolute">
-            <div className="Rectangle1 w-[116px] h-[101.54px] left-[-0px] top-[4.23px] absolute bg-white rounded-3xl border border-white" />
-            <div className="ShoppingBasket w-[112.97px] h-[110px] left-[1.52px] top-[-0px] absolute justify-center items-start gap-2.5 inline-flex" />
-        </div>
-        <div className="Hearts pr-[18.89px] left-[60%] top-[30%] absolute justify-start items-start gap-[12.52px] inline-flex">
-            <div className="H5 w-[50.81px] h-[47.38px] relative">
-            <div className="Group w-[33.75px] h-[13.79px] left-[3.95px] top-[3.92px] absolute">
-            </div>
-            </div>
-            <div className="H4 w-[50.81px] h-[47.38px] relative">
-            <div className="Group w-[33.75px] h-[13.79px] left-[3.95px] top-[3.92px] absolute">
-            </div>
-            </div>
-            <div className="H3 w-[50.81px] h-[47.38px] relative">
-            <div className="Group w-[33.75px] h-[13.79px] left-[3.95px] top-[3.92px] absolute">
-            </div>
-            </div>
-            <div className="H2 w-[50.81px] h-[47.38px] relative">
-            <div className="Group w-[33.75px] h-[13.79px] left-[3.95px] top-[3.92px] absolute">
-            </div>
-            </div>
-            <div className="H1 w-[50.81px] h-[47.38px] relative">
-            <div className="Group w-[33.75px] h-[13.79px] left-[3.95px] top-[3.92px] absolute">
-            </div>
-            </div>
-        </div>
-        <img className="StSmall507x507Pad600x600F8f8f8RemovebgPreview1 w-[708px] h-[674px] left-[378px] top-[153px] absolute" src="https://via.placeholder.com/708x674" />
-        <div className="Money w-[282.86px] h-[100.17px] left-[2%] top-[90%] absolute">
-        <div className=" w-[266.86px] h-[100.17px] left-[16px] top-0 absolute text-white text-[64px] font-normal font-['Actor']">{x}</div>
-            <img className="KawaiiBread w-[102.75px] h-[72.96px] left-[151px] top-[4.81px] absolute" src="https://via.placeholder.com/103x73" />
-            <div className="Rectangle2 w-64 h-[82.70px] left-0 top-0 absolute bg-zinc-300 bg-opacity-0 rounded-[32px] border-4 border-white" />
-        </div>
+        <div className="Money w-[30%] h-[60%] left-[1%] top-[90%] absolute">
+            <div className=" w-[8vw] h-[vh] left-[2vw] top-[2.5%] absolute text-center text-white text-5xl font-normal font-{font}">{score}</div>
+            <img className="KawaiiBread w-[6vw] h-[10vh] left-[30%] top-[-1%] absolute" src={bread} alt="oks" />
+            <div className="Rectangle2 w-[15vw] h-[9vh] left-0 top-0 absolute bg-zinc-300 bg-opacity-0 rounded-[32px] border-4 border-white border-opacity-50" /></div>
         </div>
     )
+    
 }
