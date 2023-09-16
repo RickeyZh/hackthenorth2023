a208c882908aa544a8959cd0eb02ab9fd88a3c54
RickeyZh <rickey.zhengg@gmail.com>
2023-09-16 18:01:16 -0400

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,15 +11,28 @@ let score = 1090;
 // Give them messages that they aren't in frame/more than 1 person in frame
 // Can print the messages in 
 export default function Home() {
+    const [isOpen, setIsOpen] = useState(false);
+    const menuPopup = () => {
+        setIsOpen(!isOpen);
+    };
     return(
         <div>
+            <P5sketch></P5sketch>
             <div className="Desktop2 flex w-[100%] h-[100%] absolute bg-slate-400 overflow-hidden ">
 
+
+            <button onClick={menuPopup}>Click me</button>{
+                isOpen && (
+                    <div>
+                     Hello
+                    </div> 
+                )
+            }
             <div className="Shop w-[6%] h-[12%] left-[92%] top-[2%] absolute align-middle">
                 <div className="Rectangle1 w-[100%] h-[100%] absolute bg-white rounded-3xl border border-white" />
-                <img className="asdd w-[7vw] h-[8vh] left-0 top-[65%] absolute" src={basket} />     
-            </button >
+                <img className="asdd w-[7vw] h-[8vh] left-0 top-[15%] absolute" src={basket} />     
 
+            </div>
             <div className="relative w-[100%] h-[100%] top-[91%] left-[80%]">
                 <img className="absolute w-[51px] h-[47px] top-0 left-0" alt="H" src={heart} />
                 <img className="absolute w-[51px] h-[47px] top-0 left-[63px]" alt="H" src={heart} />
@@ -28,13 +41,6 @@ export default function Home() {
                 <img className="absolute w-[51px] h-[47px] top-0 left-[253px]" alt="H" src={heart} />
             </div>
 
-            <div className="w-[708px] h-[674px]" id="image">
-                <img
-                    className="fixed w-[708px] h-[674px] top-0 left-0 object-cover"
-                    alt="St small pad"
-                    src={bread}
-                />
-            </div>
             
             <div className="Money w-[30%] h-[60%] left-[1%] top-[90%] absolute">
                 <div className=" w-[8vw] h-[vh] left-[2vw] top-[2.5%] absolute text-center text-white text-5xl font-normal font-{font}">{score}</div>
